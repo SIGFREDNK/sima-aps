345ceda0aead5d746e37c99bfc8fe33a743d6360
Sigfred Frey Nørgaard Knudsen <sigfred@virkefeltet.dk>
2022-07-07 11:24:51 +0200

    added analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,5 @@
 import { motion, AnimatePresence } from 'framer-motion';
+import Script from 'next/script';
 import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps, router }) {
@@ -11,6 +12,18 @@ function MyApp({ Component, pageProps, router }) {
                 transition={{ duration: 0.3, type: 'linear' }}
                 key={router.route}
             >
+                <Script
+                    strategy="lazyOnload"
+                    src="https://www.googletagmanager.com/gtag/js?id=G-P75DQE390X"
+                />
+                <Script strategy="lazyOnload" id="analytics">
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+                    `}
+                </Script>
                 <Component {...pageProps} />
             </motion.div>
         </AnimatePresence>
